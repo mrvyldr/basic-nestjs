7306556d579c6a2c8bfa5bd97c6f232bfac3f47f
merve.yildir <merve.yildir@trendyol.com>
2023-09-01 10:27:08 +0300

    remove idea folders

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    console.log("hello service 2")
+    console.log("hello service")
     return 'Hello World!';
   }
   getExampleApi(): string {
